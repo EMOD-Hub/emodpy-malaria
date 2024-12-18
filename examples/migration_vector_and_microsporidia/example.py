0758cd21afce7dc22406c172a89440d06684f7da
Sharon Chen <38014764+shchen-idmod@users.noreply.github.com>
2024-12-18 10:56:14 -0800

    svetlana.titova@gatesfoundation.org
    * Update slurm examples to use centralized util functions and also use ntasks for mpirun in slurm
    
    * Removed unused comments in burnin_utils.py
    
    * Change to use platform.create_sim_directory_df(exp_id) in burnin_utils.py
    
    * Update examples to use centralized burnin_utils.py under examples dir

--- examples/migration_vector_and_microsporidia/example.py
+++ examples/migration_vector_and_microsporidia/example.py
@@ -201,11 +201,11 @@ def general_sim(selected_platform):
 
     # Set platform
     # use Platform("SLURMStage") to run on comps2.idmod.org for testing/dev work
-    if selected_platform == "COMPS":
+    if selected_platform.upper().startswith("COMPS"):
         platform = Platform("Calculon", node_group="idm_48cores", priority="Highest")
         # set the singularity image to be used when running this experiment
         task.set_sif(manifest.sif_path)
-    elif selected_platform.startswith("SLURM"):
+    elif selected_platform.upper().startswith("SLURM_LOCAL"):
         # This is for native slurm cluster
         # Quest slurm cluster. 'b1139' is guest partition for idm user. You may have different partition and acct
         platform = Platform(selected_platform, job_directory=manifest.job_directory, partition='b1139', time='10:00:00',
--- examples/utils/burnin_utils.py
+++ examples/utils/burnin_utils.py
@@ -1,13 +1,12 @@
+import copy
 import os
 from typing import List, Dict
 
-
+import pandas as pd
 from COMPS import AuthManager
-from COMPS.Data import Simulation, QueryCriteria
+from COMPS.Data import QueryCriteria, Simulation
 from idmtools.core import ItemType
 from idmtools.entities.iplatform import IPlatform
-from idmtools_platform_comps.comps_platform import COMPSPlatform
-from idmtools_platform_slurm.slurm_platform import SlurmPlatform
 
 
 def _get_serialized_filenames(num_cores, timesteps):
@@ -18,17 +17,17 @@ def _get_serialized_filenames(num_cores, timesteps):
                            for core_count in range(num_cores)]
     return serialized_list
 
-
 def _get_core_counts(sim_id, platform):
     num_cores = 1
-    if isinstance(platform, COMPSPlatform):
+    if type(platform).__name__ == 'COMPSPlatform':
         sim = platform.get_item(sim_id, ItemType.SIMULATION, raw=True)
         sim.refresh(QueryCriteria().select_children('hpc_jobs'))
         num_cores = int(sim.hpc_jobs[-1].configuration.max_cores)
-    elif isinstance(platform, SlurmPlatform):
-        # TODO: Implement this with slurm platform
-        num_cores = 1
+    elif type(platform).__name__ == 'ContainerPlatform' or type(platform).__name__ == 'SlurmPlatform':
+        num_cores = platform.ntasks if platform.ntasks is not None else 1
+        # print("num_cores:", num_cores) 
     return num_cores
+
 
 def get_workdir_from_simulations(platform: 'IPlatform', comps_simulations: List[Simulation]) -> Dict[str, str]:
     """
@@ -36,9 +35,7 @@ def get_workdir_from_simulations(platform: 'IPlatform', comps_simulations: List[
     Args:
         platform: idmtools Platform
         comps_simulations: COMPS Simulations
-
     Returns: dictionary with simid as key and filepath as value
-
     """
 
     if platform.environment.upper() == "SLURMSTAGE" or platform.environment.upper() == "CALCULON":
@@ -51,30 +48,67 @@ def get_workdir_from_simulations(platform: 'IPlatform', comps_simulations: List[
 
     return sim_work_dir
 
-def build_burnin_df(exp_id: str, platform):
+
+def create_sim_directory_map(exp_id: str, platform: 'IPlatform'):
     """
-    return dataframe which contains serialized_file_path, serialized_population_filenames
+        Return a dataframe which contains simulation's working_path and tags.
+    Args:
+        exp_id: experiment id
+        platform: idmtools platform
+    Returns:
+        dataframe
+    """
+    # Get idmtools Experiment
+    exp = platform.get_item(exp_id, ItemType.EXPERIMENT, raw=False)
+    tags_list = []
+    for sim in exp.simulations:
+        tags = copy.deepcopy(sim.tags)
+        tags.update(dict(simid=sim.id))
+        tags_list.append(tags)
+    df = pd.DataFrame(tags_list)
+
+    if len(df) != len(exp.simulations):
+        print(f'Warning: not all jobs in {exp_id} succeeded', ':', )
+        print(len(exp.simulations) - len(df), 'unsucceeded')
+
+    simulations = exp.simulations
+    dir_list = []
+    for sim in simulations:
+        dir_dict = {"simid": str(sim.id), "serialized_file_path": platform.get_directory(sim)}
+        dir_list.append(dir_dict)
+
+    df_dir = pd.DataFrame(dir_list)
+    df = pd.merge(left=df, right=df_dir, on='simid')
+
+    return df
+
+
+def build_burnin_df(exp_id: str, platform, serialize_days):
+    """
+    return dataframe which contains outpath, serialized_population_filenames
     Args:
         exp_id:
         platform:
-
+        serialize_days:
     Returns:
         dataframe:
-        Run_Number | Serialization_Time_Steps | task_type | sweep_tag | simid | serialized_file_path|Num_Cores|Serialized_Population_Filenames
+        Run_Number | Serialization_Time_Steps | task_type | sweep_tag | simid | outpath|Num_Cores|Serialized_Population_Filenames
     Note, Serialized_Population_Filenames depends on n_cores. if n_cores = 2, Serialized_Population_Filenames look
     like these: state-00050-000.dtk, state-00050-001.dtk
     """
 
-    df = platform.create_sim_directory_map(exp_id)
+    df = platform.create_sim_directory_df(exp_id)
     # add Num_Cores to df
     df["Num_Cores"] = df["simid"].apply(_get_core_counts, platform=platform)
 
-    try:
-        burnin_length_in_days = int(df["Serialization_Time_Steps"].iloc[0].strip('[]'))
-    except AttributeError:
+    #try:
+    burnin_length_in_days = serialize_days #int(df["Serialization_Time_Steps"].iloc[0].strip('[]'))
+    #except AttributeError:
         # different versions of pandas save this as either a string or a list
-        burnin_length_in_days = df["Serialization_Time_Steps"].iloc[0][-1]
+        #burnin_length_in_days = df["Serialization_Time_Steps"].iloc[0][-1]
 
     df["Serialized_Population_Filenames"] = df["Num_Cores"].apply(_get_serialized_filenames, timesteps=burnin_length_in_days)
     df = df.reset_index(drop=True)
+    # Assuming df is your DataFrame
+    df.to_csv('serilized_output.csv', index=False)
     return df
